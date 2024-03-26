6cb6b740c4ca125c12daf8272a2957506274a614
MukaDeveloper <muka.bertoni@gmail.com>
2024-03-25 22:28:10 -0300

    Avaliação finaizada

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onDiceRoll(object sender, EventArgs e)
+        {
+            int faces = (int) picker.SelectedItem;
+            Random rnd = new Random();
+            int random = rnd.Next(1, faces + 1);
+            diceValueLabel.Text = random.ToString();
+        }
+    }
+
+}
